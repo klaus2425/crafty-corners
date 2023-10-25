b25c3189f3f4f43c789f7e4a078f848d9d698920
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-10-26 00:13:36 +0800

    Create Layouts

---
+++ react/src/layouts/DefaultLayout.jsx
@@ -0,0 +1,7 @@
+export default DefaultLayout = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
---
+++ react/src/layouts/GuestLayout.jsx
@@ -0,0 +1,7 @@
+export default GuestLayout = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
