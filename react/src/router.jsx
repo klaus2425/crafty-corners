f651e3cf6631b9c03f523d12038b66d5662bb053
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-10-25 23:28:06 +0800

    Create Routes for Login, Signup and Users

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,7 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
-    
+    {
+        path: '/login',
+        element: <Login />
+    }
+    {
+        path: '/signup',
+        element: <Signup />
+    }
+    {
+        path: '/users',
+        element: <Users />
+    }
 
 ]);
 
