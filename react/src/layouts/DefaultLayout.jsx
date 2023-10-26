d2932bd2704e3be10aa83cd9b58b6f3fc30f2430
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-10-26 08:06:11 +0800

    Update DefaultLayout.jsx

--- react/src/layouts/DefaultLayout.jsx
+++ react/src/layouts/DefaultLayout.jsx
@@ -1,11 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../context/ContextProvider";
 
 const DefaultLayout = () => {
+    const {user, token} = useStateContext();
+
+    if (!token) {
+        return <Navigate to='./login' />;
+    }
+
     return (
         <div>
             <Outlet />
         </div>
-    )
+    );
 }
 
 export default DefaultLayout;
