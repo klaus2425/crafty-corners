fd02e8383d15597eff5008a5adb841f691782b94
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-10-26 22:06:30 +0800

    Update main.tsx

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import { ContextProvider } from './context/ContextProvider';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ContextProvider>
       <RouterProvider router={router} />
