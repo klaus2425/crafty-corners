161bd95514215a29a837455225b37a06f03bb46c
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-10-26 23:23:43 +0800

    Update navbar for GuestLayout

--- react/src/layouts/GuestLayout.jsx
+++ react/src/layouts/GuestLayout.jsx
@@ -9,7 +9,14 @@ const GuestLayout = () => {
     
     return (
         <div>
-            <div className="navbar"></div>
+            <div className='navbar'>
+                <div className='navbar title'>
+                    <img src='../../public/Logo.png'/>
+                    <h1>Crafty Corners</h1>
+                </div>
+                <input placeholder="Search for Discussions or Topics"/>
+                <button>Login</button>
+            </div>
             <Outlet />
         </div>
     )
