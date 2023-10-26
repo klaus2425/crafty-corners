0874d03e2c8ea5e5cbe09cd161b6aa89f30101cc
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-10-26 08:08:18 +0800

    Update GuestLayout.jsx

--- react/src/layouts/GuestLayout.jsx
+++ react/src/layouts/GuestLayout.jsx
@@ -1,6 +1,12 @@
 import { Outlet } from "react-router-dom"
+import { useStateContext } from "../context/ContextProvider";
 
 const GuestLayout = () => {
+    const {token} = useStateContext;
+    if (token) {
+        return <Navigate to='/' />
+    }
+    
     return (
         <div>
             <div>For Guest Users Only</div>
