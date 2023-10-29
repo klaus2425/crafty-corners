983cf3cbdb6dd5addcf167a9abba79247866ff4d
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-10-29 11:07:20 +0800

    Update DefaultLayout.jsx

--- react/src/layouts/DefaultLayout.jsx
+++ react/src/layouts/DefaultLayout.jsx
@@ -4,12 +4,20 @@ import { useStateContext } from "../context/ContextProvider";
 const DefaultLayout = () => {
     const {user, token} = useStateContext();
 
-    if (!token) {
+    if (token) {
         return <Navigate to='./Landing' />;
     }
 
     return (
         <div>
+            <div className='navbar'>
+                <div className='navbar title'>
+                    <img src='Logo.png'/>
+                    <h1>Crafty Corners</h1>
+                </div>
+                <input type='text' placeholder="Search for Discussions or Topics"/>
+                <button></button>
+            </div>
             <Outlet />
         </div>
     );
