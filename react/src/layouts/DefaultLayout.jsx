45fd956706ce6d131d06cba983fd9b4706f60272
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-10-29 14:06:11 +0800

    Move Add Post button to Profile Section

--- react/src/layouts/DefaultLayout.jsx
+++ react/src/layouts/DefaultLayout.jsx
@@ -17,13 +17,13 @@ const DefaultLayout = () => {
                 </div>
                 <div className="search-post">
                     <input type='text' placeholder="Search for Discussions or Topics"/>
-                    <button className='add-post'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                        <path d="M12 20.6154L12 3.30768M3.34619 11.9615H20.6539" stroke="#677186" stroke-width="5.76923" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                    </button>
                 </div>
                 <div className="profile">
+                     <button className='add-post'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M12 20.6154L12 3.30768M3.34619 11.9615H20.6539" stroke="#677186" stroke-width="5.76923" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
                     <h3>Hi, Jaycie</h3>
                     <img src="/Jaycie.png" alt="Profile Picture" />
                 </div>
