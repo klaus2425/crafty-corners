3ce0b6f6bd5ed6b26b55e48f0a9207da141167fe
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-11-05 13:51:07 +0800

    Update LoginModal.jsx

--- react/src/layouts/GuestLayout.jsx
+++ react/src/layouts/GuestLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
 import { useStateContext } from "../context/ContextProvider";
 import '../styles/index.scss'
+import { Navigate } from "react-router-dom";
 import Navbar from '../components/Navbar';
 
 
