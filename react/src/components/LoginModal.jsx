9ea3ebcf78533d127a61f0771253b661f6010a31
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-11-05 15:43:10 +0800

    Update Login Modal

--- react/src/components/LoginModal.jsx
+++ react/src/components/LoginModal.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
+
+
 export default function LoginModal(props) {
     if(!props.isOpen) return null;
     return (
-        <div className='overlay'>
-            <div className='modal'>
-                <div>LoginModal</div>
-                <button onClick={() => props.setIsOpen(false)}></button>
+        <>
+            <div className='overlay'>
+                <div className='modal'>
+                    <div>LoginModal</div>
+                    <button onClick={() => props.setIsOpen(false)}>Button</button>
+                </div>
             </div>
-        </div>
+        </>
        
   )
 }
