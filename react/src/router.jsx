9dc779cf6d151706eccf3b081226307f10fdc568
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-11-05 10:32:37 +0800

    Add views to sidebar links

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import Signup from './views/Signup';
 import Login from './views/Login';
-import Users from './views/Users';
 import Home from './views/Home';
 import NotFound from './views/NotFound';
 import DefaultLayout from './layouts/DefaultLayout';
 import GuestLayout from "./layouts/GuestLayout";
 import Landing from './views/Landing';
+import Notifications from './views/Notifications';
 
 const router = createBrowserRouter([
     {
@@ -22,9 +22,10 @@ const router = createBrowserRouter([
                 element: <Home />
             },
             {
-                path: '/Home',
-                element: <Users />
+                path: '/Notifications',
+                element: <Notifications />
             },
+
         ],
     },
     {
---
+++ react/src/views/Mentors.jsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren } from 'react';
+import { Link, useOutletContext } from 'react-router-dom'
+
+
+const UserFeed =  () => {
+    const [name] = useOutletContext();
+    return (
+        <div className="authenticated-container">
+            <div className="feed">
+            User Feed
+            </div>
+            <div className="recommended">
+            recommended section
+            </div>
+        </div>
+    )
+}
+
+export default UserFeed;
