3121e92ca08c7c297371ea1bc2719120b39e491e
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-11-06 22:12:02 +0800

    Update ContextProvider.jsx

--- react/src/context/ContextProvider.jsx
+++ react/src/context/ContextProvider.jsx
@@ -13,7 +13,8 @@ export const ContextProvider = ({children}) => {
     const [user, setUser] = useState({});
     const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
     const [isOpen, setIsOpen] = useState(false);
-
+    const [isSignUpOpen, setIsSignUpOpen] = useState(false);
+    
     const setToken = (token) =>  {
         _setToken(token)
         if(token) {
@@ -30,6 +31,9 @@ export const ContextProvider = ({children}) => {
             setUser,
             setToken,
             setIsOpen,
+            isSignUpOpen,
+            setIsSignUpOpen,
+
         }}>
             {children}
         </StateContext.Provider>
