198d03cf442da5253fd482b0cc4549b75fbac95e
Nixon <66957205+Overseer24@users.noreply.github.com>
2023-11-11 14:32:13 +0800

    Create RegisterRequest.php

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'middle_name' => 'nullable|string',
+            'user_name' => 'required|string|unique:users,user_name',
+            'birthday' => 'nullable|date',
+            'street_address' => 'nullable|string',
+            'municipality' => 'nullable|string',
+            'province' => 'nullable|string',
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8)
+                    ->letters()
+                    ->symbols()
+                    ->numbers()
+            ]
+        ];
+    }
+}
