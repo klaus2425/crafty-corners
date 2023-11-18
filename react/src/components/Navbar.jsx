dca12385e4ce5a561c007a1a19d215f075d8c204
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-11-18 14:13:23 +0800

    Update Navbar.jsx

--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { useStateContext } from "../context/ContextProvider";
 import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUpModal";
