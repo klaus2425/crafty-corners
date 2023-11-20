802415d2bec92fa750e23d2c9f335dd3c86ab3a9
Nixon <66957205+Overseer24@users.noreply.github.com>
2023-11-20 22:29:50 +0800

    Add CRUD Update for UserUpdate Profile

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public static $wrap = false;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'created_at' => $this->created_at
+
+        ];
+    }
+}
