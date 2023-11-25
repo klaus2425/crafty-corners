56d908d7a73e8656618f13f9019d550bea2d04c6
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-11-25 17:33:41 +0800

    Update Navbar.jsx

--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -1,20 +1,23 @@
 import { useStateContext } from "../context/ContextProvider";
 import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUpModal";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axiosClient from "../axios-client";
+import DropDownItem from "./DropDownItem";
 
 const Navbar = () => {
-    const {isOpen, setIsOpen, isSignUpOpen, setIsSignUpOpen, setUser, setToken} = useStateContext();
-
-    const onLogout = (ev) => {
-        ev.preventDefault();
-
+    const {isOpen, setIsOpen, isSignUpOpen, setIsSignUpOpen, setUser, setToken, user, token} = useStateContext();
+    const [openDropDown, setOpenDropDown] = useState(false);
+    const onLogout = () => {
         axiosClient.post('/logout')
         .then(() => {
-            setUser({}),
-            setToken(null)
+            setUser({});
+            setToken(null);
+            window.location.reload();
         })
+    }    
+    const handleDropDown = () => {
+        openDropDown ? setOpenDropDown(false) : setOpenDropDown(true);
     }
 
     useEffect(() => {
@@ -24,9 +27,9 @@ const Navbar = () => {
         })
     }, []);
 
-    const {user, token} = useStateContext();
     const storageBaseUrl = import.meta.env.VITE_API_STORAGE_URL;
-    if (token){ //change to true
+    const userPicture = `${storageBaseUrl}/${user.profile_picture}`;
+    if (token){ 
         return (
             <div className='authenticated-navbar'>
                 <div className='navbar title'>
@@ -42,14 +45,12 @@ const Navbar = () => {
                         <path d="M12 20.6154L12 3.30768M3.34619 11.9615H20.6539" stroke="#677186" stroke-width="5.76923" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
                     </button>
-                    <h3>Hi, {user.first_name}</h3>
-                    <img src={`${storageBaseUrl}/${user.profile_picture}`} alt="Profile Picture" />
-                    <button id='logout_button' onClick={onLogout}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none">
-                            <path d="M8 18.9282C9.21615 19.6303 10.5957 20 12 20C13.4043 20 14.7838 19.6303 16 18.9282C17.2162 18.2261 18.2261 17.2162 18.9282 16C19.6303 14.7838 20 13.4043 20 12C20 10.5957 19.6303 9.21615 18.9282 8C18.2261 6.78385 17.2162 5.77394 16 5.0718C14.7838 4.36965 13.4043 4 12 4C10.5957 4 9.21615 4.36965 8 5.0718" stroke="#33363F" stroke-width="2"/>
-                            <path d="M2 12L1.21913 11.3753L0.719375 12L1.21913 12.6247L2 12ZM11 13C11.5523 13 12 12.5523 12 12C12 11.4477 11.5523 11 11 11V13ZM5.21913 6.3753L1.21913 11.3753L2.78087 12.6247L6.78087 7.6247L5.21913 6.3753ZM1.21913 12.6247L5.21913 17.6247L6.78087 16.3753L2.78087 11.3753L1.21913 12.6247ZM2 13H11V11H2V13Z" fill="#33363F"/>
-                        </svg>
-                    </button>
+                    <h3 id="navbar-header">Hi, {user.first_name}</h3>
+                    <img className="navbar-img" src={userPicture} alt="Profile Picture" onClick={handleDropDown} />
+
+                    {openDropDown && (
+                        <DropDownItem userData={user} logout={onLogout} picture = {userPicture}/>
+                    )}
                 </div>
             </div>
         )
