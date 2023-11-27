f32ca1e6288e023d5b7b2dee54e36147a1240ebf
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-11-27 19:47:24 +0800

    Update EditUser.jsx

--- react/src/Pages/Admin/EditUser.jsx
+++ react/src/Pages/Admin/EditUser.jsx
@@ -4,11 +4,12 @@ import axiosClient from "../../axios-client";
 
 const EditUser = () => {
     let {id} = useParams();
+    const storageBaseUrl = import.meta.env.VITE_API_STORAGE_URL;
     const[user, setUser] = useState({});
-    const [currentUser, setCurrentUser] = useState({});
+    const [imageChange, setImageChange] = useState(false);
     const [loading, setLoading] = useState(false);
-    console.log(id);
     const [image, setImage] = useState();
+    if (!image) {setImage('/avatar.jpg')}
 
     if(id) {
         useEffect(() => {
@@ -16,6 +17,7 @@ const EditUser = () => {
                   .then(({ data }) => {
                     setUser(data);
                     console.log(data);
+                   setImage(storageBaseUrl+data.profile_picture)
                   })
                   .catch(() => {
                   })
@@ -25,26 +27,24 @@ const EditUser = () => {
     const handleChange = (ev) => {
         setImage(URL.createObjectURL(ev.target.files[0]));
         setImageChange(true);
-        setCurrentUser({...currentUser, profile_picture: ev.target.files[0]})
+        setUser({...user, profile_picture: ev.target.files[0]})
     };
 
     const onSubmit = (ev) => {
         ev.preventDefault();
-
         if(!imageChange) {
             const formData = new FormData();
-                    
             formData.append("_method", "PUT");
-            formData.append('first_name', currentUser.first_name);
-            formData.append('middle_name', currentUser.middle_name);
-            formData.append('last_name', currentUser.last_name); 
-            formData.append('email', currentUser.email); 
-            formData.append('password', currentUser.password); 
-            formData.append('birthday', currentUser.birthday); 
-            formData.append('street_address', currentUser.street_address); 
-            formData.append('municipality', currentUser.municipality); 
-            formData.append('province', currentUser.province); 
-            axiosClient.post(`users/${currentUser.id}`, formData)
+            formData.append('first_name', user.first_name);
+            formData.append('middle_name', user.middle_name);
+            formData.append('last_name', user.last_name); 
+            formData.append('email', user.email); 
+            formData.append('password', user.password); 
+            formData.append('birthday', user.birthday); 
+            formData.append('street_address', user.street_address); 
+            formData.append('municipality', user.municipality); 
+            formData.append('province', user.province); 
+            axiosClient.post(`users/${id}`, formData)
                 .then((res) => {
                     console.log(res.data); 
                     window.location.reload();
@@ -60,12 +60,12 @@ const EditUser = () => {
             const formData = new FormData();
                 
             formData.append("_method", "PUT");
-            for (const key in currentUser) {
-                formData.append(key, currentUser[key]);
-                console.log(currentUser[key]);
+            for (const key in user) {
+                formData.append(key, user[key]);
+                console.log(user[key]);
               }
         
-            axiosClient.post(`users/${currentUser.id}`, formData)
+            axiosClient.post(`users/${id}`, formData)
                 .then((res) => {
                     console.log(res.data); 
                     window.location.reload();
@@ -73,7 +73,7 @@ const EditUser = () => {
                 .catch(err => {
                 const response = err.response;
                 setLoading(false);
-                setCurrentUser(data);
+                setUser(data);
                 setImage(storageBaseUrl+data.profile_picture)
             })
             .catch(() =>{
@@ -86,7 +86,7 @@ const EditUser = () => {
     
     return (
         <div className="edit-user-container">
-            <form enctype="multipart/form-data" onSubmit={onSubmit}>
+            <form className="edit-form" encType="multipart/form-data" onSubmit={onSubmit}>
                 <div className="edit-left">
                     <div className="edit-labels">
                         <label>Username</label>
@@ -99,23 +99,27 @@ const EditUser = () => {
                         <label>Street Address</label>
                         <label>Municipality</label>
                         <label>Province</label>
-
                     </div>
                     <div className="edit-inputs">
-                        <input />
-                        <input />
-                        <input />
-                        <input />
-                        <input />
-                        <input />
-                        <input />
-                        <input />
-                        <input />
-                        <input />
+                        <input type="text" value={user.user_name} onChange={ev => setUser({...user, user_name: ev.target.value})}/>
+                        <input type="text" value={user.first_name} onChange={ev => setUser({...user, first_name: ev.target.value})}/>
+                        <input type="text" value={user.middle_name} onChange={ev => setUser({...user, middle_name: ev.target.value})}/>
+                        <input type="text" value={user.last_name} onChange={ev => setUser({...user, last_name: ev.target.value})}/>
+                        <input type="text" value={user.email} onChange={ev => setUser({...user, email: ev.target.value})}/>
+                        <input type="date" value={user.birthday} onChange={ev => setUser({...user, birthday: ev.target.value})}/>
+                        <input type="text" value={user.gender} onChange={ev => setUser({...user, gender: ev.target.value})}/>
+                        <input type="text" value={user.street_address} onChange={ev => setUser({...user, street_address: ev.target.value})}/>
+                        <input type="text" value={user.municipality} onChange={ev => setUser({...user, municipality: ev.target.value})}/>
+                        <input type="text" value={user.province} onChange={ev => setUser({...user, province: ev.target.value})}/>
+                        <button type="submit">Save</button>
                     </div>
                 </div>
                 <div className="edit-right">
-                    
+                    <div className="upload-picture">
+                                <img id='update-picture'src={image}/>
+                                <input id='upload-button' type="file" onChange={handleChange} />
+                                <label htmlFor='upload-button'>Upload File</label>
+                    </div>
                 </div>   
             </form>
                             
