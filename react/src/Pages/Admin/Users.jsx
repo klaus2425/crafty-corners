e9074609bfa08fd1d9047e5f1b438d17661a1c34
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-11-27 14:44:31 +0800

    Update Users.jsx

--- react/src/Pages/Admin/Users.jsx
+++ react/src/Pages/Admin/Users.jsx
@@ -1,9 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axiosClient from "../../axios-client";
+import { Link } from "react-router-dom";
 
 const Users = () => {
-
+    const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(false);
-    
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    const getUsers = () => {
+        setLoading(true)
+        axiosClient.get('/users')
+          .then(({ data }) => {
+            setLoading(false)
+            setUsers(data.data)
+          })
+          .catch(() => {
+            setLoading(false)
+          })
+      }
+      console.log(users);
+
     return ( 
         <div className='users-table'>
             <table>
@@ -16,10 +34,37 @@ const Users = () => {
             <th>Actions</th>
           </tr>
           </thead>
-          
+          {loading &&
+            <tbody>
+            <tr>
+              <td colSpan="5" class="text-center">
+                Loading...
+              </td>
+            </tr>
+            </tbody>
+          }
+          {!loading &&
+            <tbody>
+            {users.map(u => (
+              <tr key={u.id}>
+                <td>{u.id}</td>
+                <td>{u.name}</td>
+                <td>{u.email}</td>
+                <td>{u.created_at}</td>
+                <td>
+                  <Link className="btn-edit" to={'/users/' + u.id}>Edit</Link>
+                  &nbsp;
+                  <button className="btn-delete" onClick={ev => onDeleteClick(u)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+            </tbody>
+          }
         </table>
         </div>
     )
+
+
 }
 
 export default Users;
