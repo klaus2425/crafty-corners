10a862fc5e3287cc19aa37cd17f6d5e58c78fc8f
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-11-27 14:24:53 +0800

    Create Users.jsx

---
+++ react/src/Pages/Admin/Users.jsx
@@ -0,0 +1,12 @@
+
+
+const Users = () => {
+
+    return ( 
+        <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id repellendus sunt distinctio, quas est veritatis debitis perferendis consequuntur iste earum eum, natus laboriosam odit molestiae inventore deserunt ut iure reprehenderit.
+        </div>
+    )
+}
+
+export default Users;
