b599fbfb49366d72c534bfcf1169d24343f7d29a
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-11-29 22:35:08 +0800

    Update AddCommunities.jsx

--- react/src/Pages/Admin/AddCommunities.jsx
+++ react/src/Pages/Admin/AddCommunities.jsx
@@ -1,8 +1,17 @@
 
 const AddCommunities = () => {
     return (
-        <div>
-            
+        <div className="add-community-container">
+            <div className="community-form">
+                <div className="community-labels">
+                    <label htmlFor="community-name">Community Name</label>
+                    <label htmlFor="community-name">Community Description</label>
+                </div>
+                <div className="community-inputs">
+                    <input type="text" name="community-name" id="community-name" />
+                    <textarea  name="community-name" rows={6} cols={20} />
+                </div>
+            </div>
         </div>
     )
 }
