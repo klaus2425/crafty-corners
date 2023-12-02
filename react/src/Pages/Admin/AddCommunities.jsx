f546e1e34cb6b568999ef29b0230f1fd23e285a3
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-12-02 17:19:52 +0800

    Update AddCommunities.jsx

--- react/src/Pages/Admin/AddCommunities.jsx
+++ react/src/Pages/Admin/AddCommunities.jsx
@@ -1,39 +1,54 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const AddCommunities = () => {
     const [image, setImage] = useState();
     const handleChange = (ev) => {
         setImage(URL.createObjectURL(ev.target.files[0]));
-        setImageChange(true);
-        setUser({...user, profile_picture: ev.target.files[0]})
     };
 
-    return (
-        <div className="add-community-container">
-            <h1>Add a New Community</h1>
-            <div className="community-form">
-                <div className="community-labels">
-                    <label htmlFor="community-name">Community Name</label>
-                    <label htmlFor="community-name">Community Description</label>
-                </div>
-                <div className="community-inputs">
-                    <input type="text" name="community-name" id="community-name" />
-                    <textarea  name="community-name" rows={6} cols={20} />
-                </div>
+    const communityNameRef = useRef();
+    const communityDescriptionRef = useRef();
+    const communityImageRef = useRef();
 
-                <div>
-                <div className="community-right">
-                    <div className="upload-picture">
-                                <img id='update-picture'src={image}/>
-                                <input id='upload-button' type="file" onChange={handleChange} />
-                                <label htmlFor='upload-button'>Upload File</label>
-                                <span className="edit-text">File size: maximum 1 MB</span>
-                                <span className="edit-text">File extension: .JPEG, .PNG, .JPG</span>
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        const formData = new FormData();
+        formData.append('name', communityNameRef);
+        formData.append('description', communityDescriptionRef);
+        formData.append('image', communityImageRef)
+
+    }
+
+    return (
+        <form encType="multipart/form-data" onSubmit={onSubmit}>
+            <div className="add-community-container">
+                <h1>Add a New Community</h1>
+                <div className="community-form">
+                    <div className="community-labels">
+                        <label htmlFor="community-name">Community Name</label>
+                        <label htmlFor="community-name">Community Description</label>
+                    </div>
+                    <div className="community-inputs">
+                        <input ref={communityNameRef} type="text" name="community-name" id="community-name" />
+                        <textarea ref={communityDescriptionRef}  name="community-name" rows={6} cols={20} />
+                    </div>
+
+                    <div>
+                    <div className="community-right">
+                        <div className="upload-picture">
+                                    <img id='update-picture'src={image}/>
+                                    <input ref={communityImageRef}id='upload-button' type="file" onChange={handleChange} />
+                                    <label htmlFor='upload-button'>Upload File</label>
+                                    <span className="edit-text">File size: maximum 2 MB</span>
+                                    <span className="edit-text">File extension: .JPEG, .PNG, .JPG</span>
+                        </div>
+                    </div>
                     </div>
                 </div>
-                </div>
+                <button className="button">Submit</button>
             </div>
-        </div>
+        </form>
     )
 }
 
