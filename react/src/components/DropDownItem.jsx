408046781ffd396d148aaa35b5d2ea81db0e5b74
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-12-05 14:08:11 +0800

    Update DropDownItem.jsx

--- react/src/components/DropDownItem.jsx
+++ react/src/components/DropDownItem.jsx
@@ -10,7 +10,7 @@ const DropDownItem = (props) => {
             <img className='dropdown-picture'src={props.picture}></img>
             <h3>{`${props.userData.first_name} ${props.userData.last_name}`}</h3>
             <span>{props.type}</span>
-            <Link>Account Settings</Link>
+            <Link to={'/account-settings'}>Account Settings</Link>
             <button id='logout_button'onClick={props.logout}>Log Out</button>
         </div>
     );
