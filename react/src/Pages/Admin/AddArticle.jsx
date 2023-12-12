6e36f43817cf3e5a58d8dc94da1102f3f3d76606
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-12-12 20:44:31 +0800

    Update AddArticle.jsx

--- react/src/Pages/Admin/AddArticle.jsx
+++ react/src/Pages/Admin/AddArticle.jsx
@@ -1,13 +1,14 @@
 import Swal from 'sweetalert2';
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import axiosClient from '../../axios-client';
 import { useStateContext } from '../../context/ContextProvider'
-
+import {useNavigate} from 'react-router-dom'
 
 const AddArticle = () => {
     const [communities, setCommunities] = useState([]);
     const {user} = useStateContext();
-
+    const [selected, setSelected] = useState();
+    const navigate = useNavigate();
     const getCommunities = () => {
         axiosClient.get('/communities').then(({ data }) => {
             setCommunities(data.data);
@@ -22,12 +23,44 @@ const AddArticle = () => {
             }
         })
     }
+    const handleChange = (ev) => {
+        setSelected(ev.target.value);
+    }
+
+    const titleRef = useRef();
+    const authorRef = useRef();
+    const descriptionRef = useRef();
+    const linkRef = useRef();
 
     useEffect(() => {
         getCommunities();
     }, [])
-    const onSubmit = () => {
-        return null;
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        const formData = new FormData();
+        formData.append('title', titleRef.current.value);
+        formData.append('author', authorRef.current.value);
+        formData.append('description', descriptionRef.current.value);
+        formData.append('community_id', selected);
+        formData.append('user_id', user.id);
+
+        axiosClient.post('/articles', formData)
+        .then(() => navigate('/admin-articles'))
+        .catch(err => {
+            const response = err.response;
+            if (response && response.status === 422) {
+                Swal.fire({
+                    title: "Error",
+                    text: `${Object.values(response.data.errors)[0]}`,
+                    icon: "warning"
+                });
+            }
+        })
+
+
+
     }
 
     return(
@@ -36,27 +69,28 @@ const AddArticle = () => {
             <h2>Add an Article</h2>
             <form onSubmit={onSubmit}>
                 <div className="article-form">
-                    <div className="article-input">
+                    <div ref={titleRef} className="article-input">
                         <label><strong>Article Title:</strong></label>
                         <input type="text" />
                     </div>
-                    <div className="article-input">
+                    <div ref={authorRef} className="article-input">
                         <label><strong>Article Author:</strong></label>
                         <input type="text" />
                     </div>
-                    <div className="article-input">
+                    <div ref={descriptionRef} className="article-input">
                         <label><strong>Description:</strong></label>
                         <input type="textarea" />
                     </div>
-                    <div className="article-input">
+                    <div ref={linkRef} className="article-input">
                         <label><strong>Link:</strong></label>
                         <input type="textarea" />
                     </div>
                     <div className="article-input">
                         <label><strong>Community:</strong></label>
-                        <select name="communities">
+                        <select name="communities" onChange={handleChange}>
+                            <option >Select a community</option>      
                             {communities.map((community) => (
-                                <option value={community.id}>{community.name}</option>      
+                                <option key={community.id} value={community.id}>{community.name}</option>      
                             ))}
                         </select>
                     </div>
