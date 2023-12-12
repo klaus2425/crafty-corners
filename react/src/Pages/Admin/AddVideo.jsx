24c9a3bbfdc7a2492414a1dfbb1648a028b6a6f8
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2023-12-12 20:50:31 +0800

    Update AddVideo.jsx

--- react/src/Pages/Admin/AddVideo.jsx
+++ react/src/Pages/Admin/AddVideo.jsx
@@ -1,12 +1,14 @@
 import Swal from 'sweetalert2';
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import axiosClient from '../../axios-client';
 import { useStateContext } from '../../context/ContextProvider'
+import {useNavigate} from 'react-router-dom'
 
 
 const AddVideo = () => {
     const [communities, setCommunities] = useState([]);
     const {user} = useStateContext();
+    const [selected, setSelected] = useState();
 
     const getCommunities = () => {
         axiosClient.get('/communities').then(({ data }) => {
@@ -26,8 +28,38 @@ const AddVideo = () => {
     useEffect(() => {
         getCommunities();
     }, [])
-    const onSubmit = () => {
-        return null;
+
+    const handleChange = (ev) => {
+        setSelected(ev.target.value);
+    }
+    
+    const titleRef = useRef();
+    const creatorRef = useRef();
+    const descriptionRef = useRef();
+    const linkRef = useRef();
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+        const formData = new FormData();
+        formData.append('title', titleRef.current.value);
+        formData.append('creator', creatorRef.current.value);
+        formData.append('description', descriptionRef.current.value);
+        formData.append('community_id', selected);
+        formData.append('user_id', user.id);
+
+        
+        axiosClient.post('/videos', formData)
+        .then(() => navigate('/admin-videos'))
+        .catch(err => {
+            const response = err.response;
+            if (response && response.status === 422) {
+                Swal.fire({
+                    title: "Error",
+                    text: `${Object.values(response.data.errors)[0]}`,
+                    icon: "warning"
+                });
+            }
+        })
     }
 
     return(
@@ -38,24 +70,24 @@ const AddVideo = () => {
                 <div className="article-form">
                     <div className="article-input">
                         <label>Video Title:</label>
-                        <input type="text" />
+                        <input ref={titleRef} type="text" />
                     </div>
                     <div className="article-input">
                         <label>Video Creator:</label>
-                        <input type="text" />
+                        <input ref={creatorRef} type="text" />
                     </div>
                     <div className="article-input">
                         <label>Description:</label>
-                        <input type="textarea" />
+                        <input ref={descriptionRef} type="textarea" />
                     </div>
                     <div className="article-input">
                         <label>Link:</label>
-                        <input type="textarea" />
+                        <input ref={linkRef} type="textarea" />
                     </div>
                     <div className="article-input">
                         <label>Community:</label>
-                        <select name="communities">
-                            
+                        <select onChange={handleChange} name="communities">
+                            <option >Select a community</option>      
                             {communities.map((community) => (
                                 <option value={community.id}>{community.name}</option>      
                             ))}
