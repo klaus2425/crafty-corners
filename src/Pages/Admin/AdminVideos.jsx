81109aba60307d3d9f15ccd79ea274c77ba6a35c
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-01-04 18:43:48 +0800

    Update AdminVideos.jsx

--- src/Pages/Admin/AdminVideos.jsx
+++ src/Pages/Admin/AdminVideos.jsx
@@ -19,6 +19,32 @@ const AdminVideos = () => {
         })
     }
 
+    const onDeleteClick = video => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+                axiosClient.delete(`/videos/${video.id}`)
+                .then((res) => {
+                    console.log(video.id);
+                    getVideos();
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Video has been deleted",
+                        icon: "success"
+                      });
+                })
+              
+            }
+          });
+    }
+
     useEffect(() => {
         getVideos();
     }, [])
