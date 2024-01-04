1867ff61135d71ab58c2bc2fd0e76f285d007577
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-01-04 18:37:30 +0800

    Update Loading.jsx

--- src/components/utils/Loading.jsx
+++ src/components/utils/Loading.jsx
@@ -1,16 +1,6 @@
 const Loading = () => {
   return(
-    <div id="loading-container">
-      <svg viewBox="0 0 100 100">
-        <defs>
-          <filter id="shadow">
-            <feDropShadow dx="0" dy="0" stdDeviation="1.5" 
-              floodColor="#fc6767"/>
-          </filter>
-        </defs>
-        <circle id="spinner" cx="50" cy="50" r="45"/>
-      </svg>
-    </div>
+    <div class="loader"></div>
   )
 }
 
