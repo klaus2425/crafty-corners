13b9a941c62a948d001a4a953510bbe7d6526cda
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-01-05 21:36:38 +0800

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,4 +1,6 @@
 import {useNavigate} from 'react-router-dom'
+import ReactPlayer from 'react-player';
+
 
 
 const Video = (props) => {
@@ -13,14 +15,20 @@ const Video = (props) => {
     return (
         
         <div onClick={() => openLink(props.link)} className="video">
-            <div className="top">
-                <span className="title">{props.title}</span>
-                <span className="community-title">/{props.community}</span>
+            <div className="left">
+                <ReactPlayer url={props.link} controls width={100} height={50} light={true}/>
             </div>
-            <div className="bottom">
-                <span><strong>{props.creator}</strong></span>
-                <span>{props.description}</span>
+            <div className="right">
+                <div className="top">
+                    <span className="title">{props.title}</span>
+                    <span className="community-title">/{props.community}</span>
+                </div>
+                <div className="bottom">
+                    <span><strong>{props.creator}</strong></span>
+                    <span>{props.description}</span>
+                </div>
             </div>
+
         </div>
     )
 }
