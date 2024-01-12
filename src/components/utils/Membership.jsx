57445f72001f556efca391624d27f55330546c6d
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-01-12 12:58:13 +0800

    Update Membership.jsx

--- src/components/utils/Membership.jsx
+++ src/components/utils/Membership.jsx
@@ -82,8 +82,8 @@ const MembershipCheck = (props) => {
     return(
     <button className="purple-button">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-          <path d="M12 6L12 18" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round"/>
-          <path d="M18 12L6 12" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round"/>
+          <path d="M12 6L12 18" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round"/>
+          <path d="M18 12L6 12" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round"/>
       </svg>
       <span onClick={() => joinCommunity(props.community_id)} className="com-button-text">Join</span>
     </button>
