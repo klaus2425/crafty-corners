b7a244692667d57ec8302acb0ccb3b7a89a9c2ef
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-02-24 13:50:52 +0800

    Update DefaultLayout.jsx

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -3,18 +3,26 @@ import { useStateContext } from "../context/ContextProvider";
 import '../styles/index.scss';
 import Navbar from '../components/Navbar';
 import { Sidebar } from "../components/Sidebar";
+import Swal from 'sweetalert2';
 
 const DefaultLayout = () => {
-    const { user, token } = useStateContext();
+    const { user, token, setToken } = useStateContext();
+    if(user.email_verified_at === null) {
+        Swal.fire({
+            position: "center",
+            icon: "info",
+            title: "Verify your email first",
+            showConfirmButton: false,
+            timer: 1500
+          });
+        setToken(null);
+        return <Navigate to='/Landing' />;
 
-    if (!token || !user) { 
+    }
+    else if (!token || !user) { 
         return <Navigate to='/Landing' />;
     }
-    if(user.email_verified_at === null) {
-        return <Navigate to='/not-verified' />
-    }
-
-    if (user.type === 'admin') {
+    else if (user.type === 'admin') {
         return <Navigate to='/Users' />
     }
 
