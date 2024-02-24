cf4d31092fb4a4a4a96a644d2a6dc4db51418645
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-02-24 13:50:56 +0800

    Update NotVerified.jsx

--- src/Pages/NotVerified.jsx
+++ src/Pages/NotVerified.jsx
@@ -1,5 +1,27 @@
 const NotVerified = () => {
+  console.log('not verified');
+
+  const onLogout = () => {
+    axiosClient.post('/logout')
+        .then(() => {
+            setUser({});
+            setToken(null);
+            window.location.reload();
+        })
+  }
+
+  return (
+          <div style={{ height: "100dvh" }} >
+              <div>
+                  <span className='landing-title'>Crafty <br /> Corners</span>
+                  <br />
+                  <span className='landing-sub'>Cultivate you hobbies <br /> with a wide community</span>
+              </div>
+          </div>
+
+  )
 
 }
+
 
 export default NotVerified;
