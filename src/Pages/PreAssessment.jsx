ef9651cb1d86fb31ec07b47a78a65ef4ef28f834
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-02-24 11:33:58 +0800

    Update PreAssessment.jsx

--- src/Pages/PreAssessment.jsx
+++ src/Pages/PreAssessment.jsx
@@ -1,8 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompass } from '@fortawesome/free-solid-svg-icons'
+
 const PreAssessment = () => {
   return (
     <div className="authenticated-container">
       <div className="feed">
-        <h1>Hello</h1>
+        <div className='section-header'>
+            <FontAwesomeIcon icon={faCompass} />
+            <h3>Pre-assessment</h3>
+        </div>
       </div>
       <div className="recommendation">
       </div>
