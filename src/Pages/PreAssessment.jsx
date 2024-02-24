8bf8181c8e31a0092455819044399ad0f125e29b
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-02-24 10:40:21 +0800

    Create PreAssessment.jsx

---
+++ src/Pages/PreAssessment.jsx
@@ -0,0 +1,13 @@
+const PreAssessment = () => {
+  return (
+    <div className="authenticated-container">
+      <div className="feed">
+        <h1>Hello</h1>
+      </div>
+      <div className="recommendation">
+      </div>
+    </div>
+  )
+}
+
+export default PreAssessment;
