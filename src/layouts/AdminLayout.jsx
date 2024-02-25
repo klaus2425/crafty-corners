b178f99dce1a01af7e004bf6f210ead06c963250
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-02-25 11:33:02 +0800

    Update AdminLayout.jsx

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -32,6 +32,8 @@ const AdminLayout = () => {
                         <Link id="2" className={active === "2" ? "active" : undefined} to="/admin-communities" onClick={handleClick}>Communities</Link>
                         <Link id="3" className={active === "3" ? "active" : undefined} to="/admin-articles" onClick={handleClick}>Articles</Link>
                         <Link id="4" className={active === "4" ? "active" : undefined} to="/admin-videos" onClick={handleClick}>Videos</Link>
+                        <Link id="5" className={active === "5" ? "active" : undefined} to="/admin-videos" onClick={handleClick}>Posts</Link>
+                        <Link id="6" className={active === "6" ? "active" : undefined} to="/admin-videos" onClick={handleClick}>Mentors</Link>
 
                     </div>
                     <div className="bottom">
