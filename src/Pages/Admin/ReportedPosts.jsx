e6a474d617aff7da5fe42a6e47f2e328b7d39cd9
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-02-27 18:31:42 +0800

    Update ReportedPosts.jsx

---
+++ src/Pages/Admin/ReportedPosts.jsx
@@ -0,0 +1,12 @@
+
+const ReportedPosts = () => {
+  return (
+    <div className="communities-container">
+      <div className="top-section">
+        <h2>Posts for review</h2>
+      </div>
+    </div>
+  )
+}
+
+export default ReportedPosts;
