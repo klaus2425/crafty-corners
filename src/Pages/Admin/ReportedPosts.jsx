4d1478ffa29c2ca2143cdd8afb0afd8bef000756
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-02-28 10:33:38 +0800

    Update ReportedPosts.jsx

--- src/Pages/Admin/ReportedPosts.jsx
+++ src/Pages/Admin/ReportedPosts.jsx
@@ -3,7 +3,8 @@ const ReportedPosts = () => {
   return (
     <div className="communities-container">
       <div className="top-section">
-        <h2>Posts for review</h2>
+      <h2>Posts for review</h2>
+      <div className='report-count'>5 <br />Reported posts</div>
       </div>
     </div>
   )
