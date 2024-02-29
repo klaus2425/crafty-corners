e9638abbe5f8bebd60f4f623b418c793e945d005
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-02-29 08:48:55 +0800

    Update axios-client.js

--- src/axios-client.js
+++ src/axios-client.js
@@ -4,18 +4,21 @@ import axios from "axios";
 const axiosClient = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
     timeout: 60000,
+    withCredentials: true,
     headers: {
         Accept: "application/json"
     }
 });
 
-const csrfToken = await axiosClient.get('/sanctum/csrf-cookie', {
-    baseURL: 'http://192.168.1.108:8000/',
-})
+// const csrfToken = await axiosClient.get('/sanctum/csrf-cookie', {
+//     baseURL: 'http://192.168.1.108:8000/',
+// }).then((res) => {
+// })
 
 axiosClient.interceptors.request.use((config) => {
     const token = localStorage.getItem('ACCESS_TOKEN');
     config.headers.Authorization = `Bearer ${token}`
+    // config.headers['X-CSRF-TOKEN'] = csrfToken;
     return config;
 });
 
