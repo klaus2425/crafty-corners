399332e0b89c369f8dbcf44002f26f55f12062af
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-03-01 12:49:39 +0800

    Update ViewMentorApplication.jsx

--- src/Pages/Admin/ViewMentorApplication.jsx
+++ src/Pages/Admin/ViewMentorApplication.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import axiosClient from "../../axios-client";
 import { useParams } from "react-router-dom";
+import toast, {Toaster} from 'react-hot-toast';
+
 
 const ViewMentorApplication = () => {
   const {id} = useParams();
@@ -11,18 +13,39 @@ const ViewMentorApplication = () => {
   const handleSubmit = () => {
     console.log(dateTimeRef.current.value);
     const formData = new FormData();
-    formData.append('date_of_assessment', dateTimeRef.current.value)
+    formData.append('date_of_Assessment', dateTimeRef.current.value)
+    formData.append('status', 'For Assessment')
     axiosClient.post(`/mentor/${id}/set-assessment_date`, formData)
     .then((res) => {
       console.log(res.data);
+      toast('Asessment confirmed', {
+        duration: 1500,
+        position: "bottom-center",
+        icon: "✅",
+        style: {
+            borderRadius: "100px",
+            border: 0,
+            boxShadow: "0 0px 20px rgb(0 0 0 / 0.1)",
+        }
+
+    });
     })
-    .catch(err => console.log(err.response))
+    .catch(err => toast(err.response.data.message, {
+      duration: 1500,
+      position: "bottom-center",
+      icon: "❌",
+      style: {
+        borderRadius: "100px",
+        border: 0,
+        boxShadow: "0 0px 20px rgb(0 0 0 / 0.1)",
+      }
+
+    }))
   }
 
   const getApplicant = () => {
     axiosClient.get(`/mentorship-application/${id}`)
     .then((res) => {
-      console.log(res.data.data);
       setApplicant(res.data.data)
     })
   }
@@ -35,11 +58,12 @@ const ViewMentorApplication = () => {
 
   return (
     <div className="communities-container">
+      <Toaster />
       <div className="top-section">
         <h2>Applicant's Information</h2>
       </div>
       <div className="applicant-image">
-        <img src={`${storageBaseUrl}`} alt="" />
+        <img src={`${storageBaseUrl + applicant.user?.profile_picture}`} alt="" />
       </div>
       <div className="applicant-info-container">
         <div className="left">
@@ -60,11 +84,14 @@ const ViewMentorApplication = () => {
           <div>Specialization</div>
           <input type="text" name="" id="" readOnly value={applicant.specialization} />
           <div>Select Date for Assessment</div>
-          <input ref={dateTimeRef} type="datetime-local" name="" id="" value={applicant?.date_of_assessment} />
+          <input ref={dateTimeRef} onChange={ev => setApplicant({...applicant, date_of_Assessment: ev.target.value})} type="datetime-local" name="" id="" value={applicant?.date_of_Assessment} />
         </div> 
       </div>
       <div className="application-bottom">
         <button className="purple-button" onClick={handleSubmit}>
+          Set Assessment
+        </button>
+        <button className="green-button">
           Confirm Application
         </button>
         <button className="red-button">
