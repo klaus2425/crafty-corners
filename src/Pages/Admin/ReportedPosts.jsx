e000dedb09bd516daf44da2790e6617eb02c3680
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-03-05 11:41:44 +0800

    Update Report

--- src/Pages/Admin/ReportedPosts.jsx
+++ src/Pages/Admin/ReportedPosts.jsx
@@ -37,9 +37,11 @@ const ReportedPosts = () => {
             <div key={u.id} className="community-item">
               <div className="community-item-details" >
                 <div className="community-details-top">
+                  {console.log(u.is_resolved === 0 ? 'red' : 'green')}
                   <span><strong>Post Title: <br /> </strong>{u.post?.title} </span>
                   <span><strong>Reason for reporting:  <br /></strong>{u.reason}</span>
                   <span className='desc'><strong>Description:  <br /></strong>{u.description}</span>
+                  <span><strong>Status:  <br /></strong><span className={u.is_resolved === 0 ? 'red' : 'green'}>{u.is_resolved=== 0 ? 'Unresolved' : 'Resolved'}</span></span>
                 </div>
                 <div className="buttons-community">
                   <Link to={'/view-reported-post/' + u.post_id+'/'+u.id} className="orange-button">View Post</Link>
