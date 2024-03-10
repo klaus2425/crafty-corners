d001c5066c6213a08d6ff507c8f4f573724394fe
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-03-10 11:16:56 +0800

    Update HobbyistProfile.jsx

--- src/Pages/HobbyistProfile.jsx
+++ src/Pages/HobbyistProfile.jsx
@@ -21,7 +21,6 @@ const HobbyistProfile = () => {
   const [currentUser, setCurrentUser] = useState([]);
   const [hasMore, setHasMore] = useState(true);
   const [pageIndex, setPageIndex] = useState(1);
-  const [messageModalOpen, setMessageModalOpen] = useState(false);
   const { id } = useParams();
   if (user.id == id) {
     navigate('/profile');
@@ -53,6 +52,15 @@ const HobbyistProfile = () => {
         setCurrentUser(res.data.data)
       })
   }
+  const sendMessage = () => {
+    axiosClient.post(`/conversation/${currentUser.id}`)
+    .then(res => {
+      console.log('Conversation',res.data);
+      navigate(`/conversation/${res.data.data.id}/${currentUser.id}`);
+    });
+
+
+  }
 
   useEffect(() => {
     getUser();
@@ -62,7 +70,6 @@ const HobbyistProfile = () => {
     <div className="authenticated-container">
       <div className="feed">
         <div className='section-header'>
-          <SendMessageModal receiver_id={currentUser.id} messageModalOpen={messageModalOpen} setMessageModalOpen={setMessageModalOpen} />
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
             <path d="M19.6515 19.4054C20.2043 19.2902 20.5336 18.7117 20.2589 18.2183C19.6533 17.1307 18.6993 16.1749 17.4788 15.4465C15.907 14.5085 13.9812 14 12 14C10.0188 14 8.09292 14.5085 6.52112 15.4465C5.30069 16.1749 4.34666 17.1307 3.74108 18.2183C3.46638 18.7117 3.79562 19.2902 4.34843 19.4054C9.39524 20.4572 14.6047 20.4572 19.6515 19.4054Z" fill="#222222" />
             <circle cx="12" cy="8" r="5" fill="#222222" />
@@ -72,7 +79,7 @@ const HobbyistProfile = () => {
         <div className="card">
           <div className='profile-card'>
             <div className='send-profile-button'>
-              <span onClick={() => setMessageModalOpen(!messageModalOpen)} className='purple-button'><FontAwesomeIcon icon={faMessage} /> <span className="button-text">Send a Message</span></span>
+              <span onClick={sendMessage} className='purple-button'><FontAwesomeIcon icon={faMessage} /> <span className="button-text">Send a Message</span></span>
             </div>
             <div className='profile-details'>
               <div className='left'>
