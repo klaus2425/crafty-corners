adc5ddc3fde3728931edb77ae6f79e0fa7575d6d
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-03-14 16:04:20 +0800

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -71,7 +71,7 @@ export const Sidebar = () => {
             </div>
             <div className='sidebar-lower'>
 
-                <div className="sidebar-link" onClick={() => navigate('/communities')} >
+                <div className="sidebar-link" onClick={() => navigate(`/communities?uid=${user.id}`)} >
 
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                         <circle cx="12" cy="9" r="4" fill="#33363F" />
