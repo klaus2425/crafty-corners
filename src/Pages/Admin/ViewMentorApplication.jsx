7fbf79b725832e9758048599ef7798afc7e339f3
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-03-15 22:52:53 +0800

    Update ViewMentorApplication.jsx

--- src/Pages/Admin/ViewMentorApplication.jsx
+++ src/Pages/Admin/ViewMentorApplication.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import axiosClient from "../../axios-client";
 import { useParams } from "react-router-dom";
-import toast, {Toaster} from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import Swal from "sweetalert2";
 
 const ViewMentorApplication = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const [applicant, setApplicant] = useState({});
   const dateTimeRef = useRef();
   const storageBaseUrl = import.meta.env.VITE_API_STORAGE_URL;
@@ -16,31 +16,31 @@ const ViewMentorApplication = () => {
     formData.append('date_of_Assessment', dateTimeRef.current.value)
     formData.append('status', 'For Assessment')
     axiosClient.post(`/mentor/${id}/set-assessment_date`, formData)
-    .then((res) => {
-      console.log(res.data);
-      toast('Asessment date set!', {
-        duration: 1500,
-        position: "bottom-center",
-        icon: "✅",
-        style: {
+      .then((res) => {
+        console.log(res.data);
+        toast('Asessment date set!', {
+          duration: 1500,
+          position: "bottom-center",
+          icon: "✅",
+          style: {
             borderRadius: "100px",
             border: 0,
             boxShadow: "0 0px 20px rgb(0 0 0 / 0.1)",
+          }
+
+        });
+      })
+      .catch(err => toast(err.response.data.message, {
+        duration: 1500,
+        position: "bottom-center",
+        icon: "❌",
+        style: {
+          borderRadius: "100px",
+          border: 0,
+          boxShadow: "0 0px 20px rgb(0 0 0 / 0.1)",
         }
 
-    });
-    })
-    .catch(err => toast(err.response.data.message, {
-      duration: 1500,
-      position: "bottom-center",
-      icon: "❌",
-      style: {
-        borderRadius: "100px",
-        border: 0,
-        boxShadow: "0 0px 20px rgb(0 0 0 / 0.1)",
-      }
-
-    }))
+      }))
   }
 
   const handleConfirmMentor = () => {
@@ -55,11 +55,14 @@ const ViewMentorApplication = () => {
     }).then((result) => {
       if (result.isConfirmed) {
         axiosClient.post(`/accept-mentorship-application/${applicant.id}`)
-        Swal.fire({
-          title: "Success!",
-          text: "User promoted to Mentor.",
-          icon: "success"
-        });
+        .then(() => {
+          Swal.fire({
+            title: "Success!",
+            text: "User promoted to Mentor.",
+            icon: "success"
+          });
+        })
+        .catch(err => console.log(err))
       }
     });
   }
@@ -75,20 +78,24 @@ const ViewMentorApplication = () => {
       confirmButtonText: "Yes"
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire({
-          title: "Success!",
-          text: "Application has been rejected.",
-          icon: "success"
-        });
+        axiosClient.post(`/accept-mentorship-application/${applicant.id}`)
+          .then(() => {
+            Swal.fire({
+              title: "Success!",
+              text: "User Application Rejected",
+              icon: "success"
+            });
+          })
+
       }
     });
   }
 
   const getApplicant = () => {
     axiosClient.get(`/mentorship-application/${id}`)
-    .then((res) => {
-      setApplicant(res.data.data)
-    })
+      .then((res) => {
+        setApplicant(res.data.data)
+      })
   }
 
 
@@ -109,24 +116,24 @@ const ViewMentorApplication = () => {
       <div className="applicant-info-container">
         <div className="left">
           <div>First Name</div>
-          <input type="text" name="" id="" readOnly value={applicant.user?.first_name}/>
+          <input type="text" name="" id="" readOnly value={applicant.user?.first_name} />
           <div>Middle Name</div>
           <input type="text" name="" id="" readOnly value={applicant.user?.middle_name} />
           <div>Last Name</div>
-          <input type="text" name="" id="" readOnly value={applicant.user?.last_name}/>
+          <input type="text" name="" id="" readOnly value={applicant.user?.last_name} />
           <div>Student ID</div>
-          <input type="text" name="" id="" readOnly value={applicant.student_id}/>
+          <input type="text" name="" id="" readOnly value={applicant.student_id} />
         </div>
         <div className="right">
           <div>Program</div>
-          <input type="text" name="" id="" readOnly value={applicant.program}/>
+          <input type="text" name="" id="" readOnly value={applicant.program} />
           <div>Community Applying For</div>
-          <input type="text" name="" id="" readOnly value={applicant.community?.name}/>
+          <input type="text" name="" id="" readOnly value={applicant.community?.name} />
           <div>Specialization</div>
           <input type="text" name="" id="" readOnly value={applicant.specialization} />
           <div>Select Date for Assessment</div>
-          <input ref={dateTimeRef} onChange={ev => setApplicant({...applicant, date_of_Assessment: ev.target.value})} type="datetime-local" name="" id="" value={applicant?.date_of_Assessment} />
-        </div> 
+          <input ref={dateTimeRef} onChange={ev => setApplicant({ ...applicant, date_of_Assessment: ev.target.value })} type="datetime-local" name="" id="" value={applicant?.date_of_Assessment} />
+        </div>
       </div>
       <div className="application-bottom">
         <button className="purple-button" onClick={handleSubmit}>
