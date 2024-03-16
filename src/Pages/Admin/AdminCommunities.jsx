f7692dc03513675553495377ae64216871eb27e9
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-03-16 17:19:32 +0800

    Remove white spaces

--- src/Pages/Admin/AdminCommunities.jsx
+++ src/Pages/Admin/AdminCommunities.jsx
@@ -11,8 +11,6 @@ import InfiniteScroll from "react-infinite-scroll-component";
 const AdminCommunities = () => {
     const storageBaseUrl = import.meta.env.VITE_API_COMMUNITIES_URL;
 
-
-
     const fetchCommunities = async (pageParams) => {
         const fetchedData = await axiosClient.get(`/communities?page=${pageParams}`);
         return { ...fetchedData.data, prevPage: pageParams };
