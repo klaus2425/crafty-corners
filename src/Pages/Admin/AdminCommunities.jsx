daf4b839012947a48c755b3448f40b718f3bb89c
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-03-16 17:16:35 +0800

    Implement infinite scroll on AdminCommunities.jsx

--- src/Pages/Admin/AdminCommunities.jsx
+++ src/Pages/Admin/AdminCommunities.jsx
@@ -1,18 +1,39 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import { useEffect, useState } from 'react'
 import axiosClient from "../../axios-client";
 import Swal from 'sweetalert2'
 import Loading from "../../components/utils/Loading";
-import GetCommunityMembers from "../../components/utils/GetCommunityMembers";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
 const AdminCommunities = () => {
     const storageBaseUrl = import.meta.env.VITE_API_COMMUNITIES_URL;
-    const [loading, setLoading] = useState(false);
-    const [communities, setCommunities] = useState([]);
 
+
+
+    const fetchCommunities = async (pageParams) => {
+        const fetchedData = await axiosClient.get(`/communities?page=${pageParams}`);
+        return { ...fetchedData.data, prevPage: pageParams };
+    }
+
+    const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+        queryKey: ['admin-communities'],
+        queryFn: ({ pageParam }) => fetchCommunities(pageParam),
+        initialPageParam: 1,
+        getNextPageParam: (lastPage) => {
+            console.log('lastPage', lastPage);
+            if (lastPage.meta.current_page + 1 > lastPage.meta.last_page) {
+                return null;
+            }
+            return lastPage.meta.current_page + 1
+        }
+    });
+
+    const fetchedCommunities = data?.pages.reduce((acc, page) => {
+        return [...acc, page.data];
+    }, [])
 
 
     const onDeleteClick = community => {
@@ -41,55 +62,48 @@ const AdminCommunities = () => {
 
     }
 
-    const getCommunities = () => {
-        setLoading(true);
-        axiosClient.get('/communities').then(({ data }) => {
-            setLoading(false);
-            setCommunities(data.data);
-        })
-    }
-
-    useEffect(() => {
-        getCommunities();
-    }, [])
-
     return (
-        <div className="communities-container">
+        <div className="communities-container" id="communities-container">
             <div className="top-section">
                 <Link className="add-community-button" to={'/add-communities'}><span><FontAwesomeIcon icon={faPlus} /></span> Add a Community</Link>
             </div>
-            <div className='users-table'>
-
-                {loading &&
-                    <Loading />
-                }
-
-                {!loading &&
-
-                    communities.map(u => (
-
-                        <div key={u.id} className="community-item">
-                            <div><img src={storageBaseUrl + u.community_photo} alt="" /></div>
-                            <div className="community-item-details" >
-                                <div className="community-details-top">
-                                    <span><strong>Community Name: <br /> </strong>{u.name}</span>
-                                    <span><strong>Description: <br /></strong>{u.description}</span>
-                                    <span><strong>Members: <br /></strong>{<GetCommunityMembers id={u.id} />}</span>
-                                </div>
-                                <div className="buttons-community">
-                                    <Link to={'/edit-community/' + u.id} className="orange-button">View Community</Link>
-                                    <button className="red-button" onClick={ev => onDeleteClick(u)}>Delete Community</button>
-                                </div>
-
-
-                            </div>
+            <div className="community-scroll" id="community-scroll">
+            {fetchedCommunities ?
+                <InfiniteScroll
+                    scrollableTarget='community-scroll'
+                    dataLength={fetchedCommunities ? fetchedCommunities.length : 0}
+                    next={fetchNextPage}
+                    hasMore={hasNextPage}
+                    loader={<Loading />}
+                    endMessage={
+                        <div style={{ textAlign: 'center' }}>
+                            <h2>End of Feed</h2>
                         </div>
-                    ))
-
-
-                }
-
+                    }>
+                    {fetchedCommunities?.map((community) =>
+                        community.map(c => (
+                            <div key={c.id} className="community-item">
+                                <div><img src={storageBaseUrl + c.community_photo} alt="" /></div>
+                                <div className="community-item-details" >
+                                    <div className="community-details-top">
+                                        <span><strong>Community Name: <br /> </strong>{c.name}</span>
+                                        <span><strong>Description: <br /></strong>{c.description}</span>
+                                        <span><strong>Members: <br /></strong>{c.members_count}</span>
+                                    </div>
+                                    <div className="buttons-community">
+                                        <Link to={'/edit-community/' + c.id} className="orange-button">View Community</Link>
+                                        <button className="red-button" onClick={() => onDeleteClick(c)}>Delete Community</button>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    )}
+                </InfiniteScroll>
+                :
+                <Loading />
+            }
             </div>
+
         </div>
     )
 }
