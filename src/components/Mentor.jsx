2c7e95c5e1bb22fdd08cd2ca5af16f9ad4cceb17
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-03-21 00:31:42 +0800

    Add navigation to mentor's profile

--- src/components/Mentor.jsx
+++ src/components/Mentor.jsx
@@ -1,11 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useStateContext } from "../context/ContextProvider";
 
 const Mentor = (mentor) => {
+  const { user } = useStateContext();
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate(`/u/${mentor.id}/?uid=${user.id}`)
+  }
 
   return (
-    <div className="mentor-item-card">
+    <div className="mentor-item-card" onClick={handleClick}>
       <img src={mentor.img} />
       <span className="mentor-name">{mentor.name}</span>
-      <span className="mentor-community">{mentor.community} Mentor</span>
+      <span>{mentor.specialization}</span>
+      <span className="mentor-community">{mentor.community}</span>
     </div>
   )
 }
