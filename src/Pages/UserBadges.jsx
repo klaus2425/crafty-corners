acb26621ecd81f2e17da141249baaea56247741f
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-03-28 16:04:28 +0800

    Create UserBadges.jsx

---
+++ src/Pages/UserBadges.jsx
@@ -0,0 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosClient from "../axios-client";
+import Loading from "../components/utils/Loading";
+import ProgressBar from "@ramonak/react-progress-bar";
+
+
+const UserBadges = () => {
+
+  const { data } = useQuery({
+    queryKey: ['badges'],
+    queryFn: () => axiosClient.get('/user-levels').then(({ data }) => (data.user_level)),
+  })
+
+  console.log('levels', data);
+  return (
+    <div className="authenticated-container">
+      <div className="feed">
+        <div className="section-header">
+          <h3>Your Badges</h3>
+        </div>
+        <div className="card" id="badges-card">
+          {
+            data ?
+              data.length > 0 ? data.map(level => (
+                <div className="level-container">
+                  <span><strong>/{level.community_name}</strong></span>
+                  <img src={`/${level.badge}`} alt="" />
+                  <ProgressBar height='1.5rem' width={120} completed={`${level.experience_points}` || 0} maxCompleted={level.next_level_experience || 0} />
+                  {level.experience_points + '/' + level.next_level_experience}
+                  <span>Level {level.level}</span>
+                </div>
+              ))
+                :
+                (<div>No badges</div>)
+              :
+              <Loading />
+          }
+        </div>
+      </div>
+      <div className="recommended"></div>
+    </div>
+  )
+}
+
+export default UserBadges;
