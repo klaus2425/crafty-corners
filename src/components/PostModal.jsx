24ebe0cc8095e5cf884fa9837739d9bc2dd870cf
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-04-03 12:50:41 +0800

    Add toggle for notifications

--- src/components/PostModal.jsx
+++ src/components/PostModal.jsx
@@ -12,6 +12,7 @@ const PostModal = (props) => {
   const { id } = useParams();
   const [fileUpload, setFileUpload] = useState(false);
   const [fileType, setFileType] = useState('');
+  const [notifiable, setNotifiable] = useState(true);
   const [image, setImage] = useState();
   const [video, setVideo] = useState();
   const [loading, setLoading] = useState(false);
@@ -36,6 +37,7 @@ const PostModal = (props) => {
       formData.append('community_id', id);
       formData.append('title', titleRef.current.value);
       formData.append('video', file);
+      formData.append('notifiable', notifiable);
       formData.append('post_type', 'video');
       axiosClient.post('/posts', formData)
         .then(() => {
@@ -59,6 +61,7 @@ const PostModal = (props) => {
       const formData = new FormData();
       formData.append('community_id', id);
       formData.append('title', titleRef.current.value);
+      formData.append('notifiable', notifiable);
       formData.append('image', file);
       formData.append('post_type', 'image');
       axiosClient.post('/posts', formData)
@@ -81,6 +84,7 @@ const PostModal = (props) => {
     else if (postType === 'text') {
       const formData = new FormData();
       formData.append('user_id', user.id);
+      formData.append('notifiable', notifiable);
       formData.append('community_id', id);
       formData.append('title', titleRef.current.value);
       formData.append('content', descriptionRef.current.value);
@@ -104,6 +108,7 @@ const PostModal = (props) => {
     else if (postType === 'url') {
       const formData = new FormData();
       formData.append('user_id', user.id);
+      formData.append('notifiable', notifiable);
       formData.append('community_id', id);
       formData.append('title', titleRef.current.value);
       formData.append('link', linkRef.current.value);
@@ -260,7 +265,29 @@ const PostModal = (props) => {
                 </div>
               </TabPanel>
             </Tabs>
-            <button type='submit' onClick={handleSubmit} className='purple-button'>Post</button>
+            <div className="post-notification-container">
+              <span onClick={() => setNotifiable(!notifiable)} className='toggle-span'>
+                {
+                  notifiable ?
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M6.44784 7.96942C6.76219 5.14032 9.15349 3 12 3V3C14.8465 3 17.2378 5.14032 17.5522 7.96942L17.804 10.2356C17.8072 10.2645 17.8088 10.279 17.8104 10.2933C17.9394 11.4169 18.3051 12.5005 18.8836 13.4725C18.8909 13.4849 18.8984 13.4973 18.9133 13.5222L19.4914 14.4856C20.0159 15.3599 20.2782 15.797 20.2216 16.1559C20.1839 16.3946 20.061 16.6117 19.8757 16.7668C19.5971 17 19.0873 17 18.0678 17H5.93223C4.91268 17 4.40291 17 4.12434 16.7668C3.93897 16.6117 3.81609 16.3946 3.77841 16.1559C3.72179 15.797 3.98407 15.3599 4.50862 14.4856L5.08665 13.5222C5.10161 13.4973 5.10909 13.4849 5.11644 13.4725C5.69488 12.5005 6.06064 11.4169 6.18959 10.2933C6.19123 10.279 6.19283 10.2645 6.19604 10.2356L6.44784 7.96942Z" stroke="#33363F" stroke-width="2" />
+                      <path d="M8 17C8 17.5253 8.10346 18.0454 8.30448 18.5307C8.5055 19.016 8.80014 19.457 9.17157 19.8284C9.54301 20.1999 9.98396 20.4945 10.4693 20.6955C10.9546 20.8965 11.4747 21 12 21C12.5253 21 13.0454 20.8965 13.5307 20.6955C14.016 20.4945 14.457 20.1999 14.8284 19.8284C15.1999 19.457 15.4945 19.016 15.6955 18.5307C15.8965 18.0454 16 17.5253 16 17" stroke="#33363F" stroke-width="2" stroke-linecap="round" />
+                    </svg>
+                    :
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M6.44784 7.96942C6.76219 5.14032 9.15349 3 12 3V3C14.8465 3 17.2378 5.14032 17.5522 7.96942L17.804 10.2356C17.8072 10.2645 17.8088 10.279 17.8104 10.2933C17.9394 11.4169 18.3051 12.5005 18.8836 13.4725C18.8909 13.4849 18.8984 13.4973 18.9133 13.5222L19.4914 14.4856C20.0159 15.3599 20.2782 15.797 20.2216 16.1559C20.1839 16.3946 20.061 16.6117 19.8757 16.7668C19.5971 17 19.0873 17 18.0678 17H5.93223C4.91268 17 4.40291 17 4.12434 16.7668C3.93897 16.6117 3.81609 16.3946 3.77841 16.1559C3.72179 15.797 3.98407 15.3599 4.50862 14.4856L5.08665 13.5222C5.10161 13.4973 5.10909 13.4849 5.11644 13.4725C5.69488 12.5005 6.06064 11.4169 6.18959 10.2933C6.19123 10.279 6.19283 10.2645 6.19604 10.2356L6.44784 7.96942Z" stroke="#33363F" stroke-width="2" />
+                      <path d="M8 17C8 17.5253 8.10346 18.0454 8.30448 18.5307C8.5055 19.016 8.80014 19.457 9.17157 19.8284C9.54301 20.1999 9.98396 20.4945 10.4693 20.6955C10.9546 20.8965 11.4747 21 12 21C12.5253 21 13.0454 20.8965 13.5307 20.6955C14.016 20.4945 14.457 20.1999 14.8284 19.8284C15.1999 19.457 15.4945 19.016 15.6955 18.5307C15.8965 18.0454 16 17.5253 16 17" stroke="#33363F" stroke-width="2" stroke-linecap="round" />
+                      <path d="M3.82849 19.6715L19.5 4.00002" stroke="#33363F" stroke-width="2" stroke-linecap="round" />
+                    </svg>
+
+                }
+
+                Toggle Notification
+              </span>
+
+
+              <button type='submit' onClick={handleSubmit} className='purple-button'>Post</button>
+            </div>
           </form>
         </div>
       </div>
