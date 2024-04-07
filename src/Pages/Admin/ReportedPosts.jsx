666434dd79288f5700408fc9d661e4567e6d0827
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-04-07 16:08:44 +0800

    Update resolving of reports

--- src/Pages/Admin/ReportedPosts.jsx
+++ src/Pages/Admin/ReportedPosts.jsx
@@ -8,8 +8,10 @@ const ReportedPosts = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const getPosts = () => {
+    setLoading(true)
     axiosClient.get('/show-all-reports')
       .then(res => {
+        setLoading(false);
         setPosts(res.data.data);
         console.log(res.data.data);
       })
@@ -39,7 +41,7 @@ const ReportedPosts = () => {
                   <span><strong>Post Title: <br /> </strong>{u.post?.title} </span>
                   <span><strong>Reason for reporting:  <br /></strong>{u.reason}</span>
                   <span className='desc'><strong>Description:  <br /></strong>{u.description}</span>
-                  <span><strong>Status:  <br /></strong><span className={u.is_resolved === 0 ? 'red' : 'green'}>{u.is_resolved=== 0 ? 'Unresolved' : 'Resolved'}</span></span>
+                  <span><strong>Status:  <br /></strong><span className={u.is_resolved ? 'green' : 'red'}>{u.is_resolved ? 'Resolved' : 'Unresolved'}</span></span>
                 </div>
                 <div className="buttons-community">
                   <Link to={'/view-reported-post/' + u.post.id+'/'+u.id} className="orange-button">View Post</Link>
