05d3f8de485c1360250f0b771c1ea2c70bb884ab
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-04-08 18:36:38 +0800

    Add useQuery

--- src/Pages/Admin/ReportedPosts.jsx
+++ src/Pages/Admin/ReportedPosts.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axiosClient from "../../axios-client";
 import Loading from "../../components/utils/Loading";
 import { Link } from "react-router-dom";
+import { useQuery } from '@tanstack/react-query';
 
 const ReportedPosts = () => {
 
@@ -19,6 +20,14 @@ const ReportedPosts = () => {
   useEffect(() => {
     getPosts();
   }, []);
+
+  const useReports = useQuery({
+    queryKey: ['reports'],
+    queryFn: () => axiosClient.get('/show-all-reports')
+      .then(({ data }) => data.data)
+  })
+
+  console.log(useReports.data);
   return (
     <div className="communities-container">
       <div className="top-section">
@@ -26,32 +35,30 @@ const ReportedPosts = () => {
       </div>
       <div className='users-table'>
 
-        {loading &&
-          <Loading />
-        }
 
-        {!loading &&
+        {useReports.data ?
 
-          posts.map(u => (
+          useReports.data?.map(u => (
             <div key={u.id} className="community-item">
               <div className="community-item-details" >
                 <div className="community-details-top">
-                <span><strong>Post ID: <br /> </strong>{u.post?.id} </span>
-
-                  <span><strong>Post Title: <br /> </strong>{u.post?.title} </span>
+                  <span><strong>Report ID: <br /> </strong>{u.id} </span>
+                  <span><strong>Post Title: <br /> </strong>{u.title} </span>
                   <span><strong>Reason for reporting:  <br /></strong>{u.reason}</span>
                   <span className='desc'><strong>Description:  <br /></strong>{u.description}</span>
                   <span><strong>Status:  <br /></strong><span className={u.is_resolved ? 'green' : 'red'}>{u.is_resolved ? 'Resolved' : 'Unresolved'}</span></span>
                 </div>
                 <div className="buttons-community">
-                  <Link to={'/view-reported-post/' + u.post.id+'/'+u.id} className="orange-button">View Post</Link>
+                  <Link to={'/view-reported-post/' + u.post.id + '/' + u.id} className="orange-button">View Post</Link>
                 </div>
 
 
               </div>
             </div>
           ))
+          :
 
+          <Loading />
 
         }
 
