cc6eff8df31d1a61865ef32ededf6d54c278a4c6
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-04-10 14:59:28 +0800

    Add updating of topics

--- src/Pages/Admin/EditCommunity.jsx
+++ src/Pages/Admin/EditCommunity.jsx
@@ -2,9 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axiosClient from "../../axios-client"
 import Swal from 'sweetalert2';
-import { AdminPosts } from "../../components/Post";
 import Loading from "../../components/utils/Loading";
-
+import toast from 'react-hot-toast';
 const EditCommunity = () => {
 
   let { id } = useParams();
@@ -14,7 +13,15 @@ const EditCommunity = () => {
   const [loading, setLoading] = useState(false);
   const [image, setImage] = useState();
   const [communityName, setCommunityName] = useState();
-  const [communityDescription, setCommunityDescription] = useState();
+  const [topics, setTopics] = useState([]);
+
+  const handleTextareaChange = (event) => {
+    const newValue = event.target.value;
+    // Assuming you want to store each line of text as a separate topic
+    const newTopics = newValue.split('\n');
+    setTopics(newTopics);
+  }
+
   const handleChange = (ev) => {
     setImage(URL.createObjectURL(ev.target.files[0]));
     setImageChange(true);
@@ -29,7 +36,6 @@ const EditCommunity = () => {
         setLoading(false);
         setImage(storageBaseUrl + data.data.community_photo);
         setCommunityName(data.data.name);
-        setCommunityDescription(data.data.description);
       })
       .catch((err) => {
         setLoading(false);
@@ -39,21 +45,13 @@ const EditCommunity = () => {
           icon: "warning"
         });
       })
-  }
-
-
-  const getCommunityPosts = () => {
-    axiosClient.get(`/communities/${id}`)
+    axiosClient.get(`/community/${id}/subtopics`)
       .then(({ data }) => {
-      })
-      .catch((err) => {
-        Swal.fire({
-          title: "Error",
-          text: `${Object.values(err.response.data.errors)[0]}`,
-          icon: "warning"
-        });
+        setTopics(data)
+        console.log(data);
       })
   }
+
 
   if (id) {
     useEffect(() => {
@@ -64,21 +62,25 @@ const EditCommunity = () => {
   const onSubmit = (ev) => {
     ev.preventDefault();
 
-    if (!imageChange && community.name !== communityName) {
+    if (!imageChange) {
       const formData = new FormData();
       formData.append("_method", "PUT");
       formData.append('name', community.name);
       formData.append('description', community.description);
+
       axiosClient.post(`communities/${id}`, formData)
         .then(() => {
-          getCommunity();
-          Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "Changes applied",
-            showConfirmButton: false,
-            timer: 1500
-          });
+          setCommunityName(community.name);
+          toast('Changes Applied', {
+            duration: 1500,
+            position: "bottom-center",
+            icon: "✅",
+            style: {
+              borderRadius: "100px",
+              border: 0,
+              boxShadow: "0 0px 20px rgb(0 0 0 / 0.1)",
+            }
+          })
         })
         .catch(err => {
           const response = err.response;
@@ -91,24 +93,28 @@ const EditCommunity = () => {
           }
         });
     }
-    else if (imageChange && community.name === communityName) {
+    else if (imageChange) {
 
       const formData = new FormData();
       formData.append("_method", "PUT");
+      formData.append('name', community.name);
       formData.append('description', community.description);
       formData.append('community_photo', community.community_photo);
-
+      formData.append('subtopics', topics);
       axiosClient.post(`communities/${id}`, formData)
         .then(() => {
-          getCommunity();
+          setCommunityName(community.name);
           setImageChange(false);
-          Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "Changes applied",
-            showConfirmButton: false,
-            timer: 1500
-          });
+          toast('Changes Applied', {
+            duration: 1500,
+            position: "bottom-center",
+            icon: "✅",
+            style: {
+              borderRadius: "100px",
+              border: 0,
+              boxShadow: "0 0px 20px rgb(0 0 0 / 0.1)",
+            }
+          })
         })
         .catch(err => {
           const response = err.response;
@@ -120,62 +126,6 @@ const EditCommunity = () => {
             });
           }
         });
-    }
-    else if (imageChange && community.name !== communityName) {
-
-      const formData = new FormData();
-      formData.append("_method", "PUT");
-      for (const key in community) {
-        formData.append(key, community[key]);
-      }
-      axiosClient.post(`communities/${community.id}`, formData)
-        .then((res) => {
-          getCommunity();
-          Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "Changes applied",
-            showConfirmButton: false,
-            timer: 1500
-          });
-        })
-        .catch(err => {
-          const response = err.response;
-          Swal.fire({
-            title: "Error",
-            text: `${Object.values(response.data.errors)[0]}`,
-            icon: "warning"
-          });
-          setLoading(false);
-          setImage(image);
-        })
-    } else if (community.description !== communityDescription) {
-      const formData = new FormData();
-      formData.append("_method", "PUT");
-      formData.append('description', community.description);
-
-      axiosClient.post(`communities/${community.id}`, formData)
-        .then(() => {
-          getCommunity();
-          Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "Changes applied",
-            showConfirmButton: false,
-            timer: 1500
-          });
-        })
-        .catch(err => {
-          const response = err.response;
-          Swal.fire({
-            title: "Error",
-            text: `${Object.values(response.data.errors)[0]}`,
-            icon: "warning"
-          });
-          setLoading(false);
-          setImage(image);
-
-        })
     }
     else {
       Swal.fire({
@@ -198,12 +148,16 @@ const EditCommunity = () => {
             <div className="community-form">
               <div className="community-input-label">
                 <div className="community-labels">
-                  <label htmlFor="community-name">Community Name</label>
-                  <label htmlFor="community-name">Community Description</label>
+                  <label style={{ marginBottom: '1.1rem' }} htmlFor="community-name">Community Name</label>
+                  <label style={{ marginBottom: '6.87rem' }} htmlFor="community-name">Community Description</label>
+                  <label htmlFor="community-name">Community Topics</label>
+
                 </div>
                 <div className="community-inputs">
                   <input value={community.name} type="text" name="community-name" id="community-name" onChange={ev => setCommunity({ ...community, name: ev.target.value })} required />
                   <textarea value={community.description} name="community-name" onChange={ev => setCommunity({ ...community, description: ev.target.value })} rows={6} cols={20} required />
+                  <textarea onChange={(ev) => handleTextareaChange(ev)} rows={6} cols={20} required />
+
                 </div>
               </div>
               <div>
