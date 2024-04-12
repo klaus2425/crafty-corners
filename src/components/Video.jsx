3e13927ef4dbff6109c8cebfabc5d40bb02f60f3
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-04-12 21:52:14 +0800

    Add gaps

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -7,8 +7,6 @@ const Video = (props) => {
     const navigate = useNavigate();
 
     const openLink = (url) => {
-        // const newWindow = window.open(url, "_blank", 'noopener,noreferrer');
-        // if (newWindow) newWindow.opener = null;
         navigate(`/v/${props.id}`)
     }
 
@@ -24,7 +22,7 @@ const Video = (props) => {
                     <span className="community-title">/{props.community}</span>
                 </div>
                 <div className="bottom">
-                    <span className='flex flex--justify-space-between'><strong>{props.creator}</strong><strong>Posted by: {props.user}</strong></span>
+                    <span className='flex flex--justify-space-between gap-1'><strong>{props.creator}</strong><strong>Posted by: {props.user}</strong></span>
                     <span>{props.description}</span>
                 </div>
             </div>
