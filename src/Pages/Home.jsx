b27c484a169c488356ba93b166664e83e07c2b57
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-04-16 11:51:18 +0800

    Adjust display on lower resolutions

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -29,7 +29,7 @@ const UserFeed = () => {
     }, [])
 
     return (
-        <div className="authenticated-container">
+        <div className="authenticated-container" id='home'>
             <div id="feed" className="feed">
                 <div className='section-header'>
                     <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
