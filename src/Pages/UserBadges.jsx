4f7ab27618c15c1de7d6601dfa9649562443f1b8
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-04-18 10:23:56 +0800

    Fix no badges display

--- src/Pages/UserBadges.jsx
+++ src/Pages/UserBadges.jsx
@@ -6,7 +6,7 @@ import ProgressBar from "@ramonak/react-progress-bar";
 
 const UserBadges = () => {
 
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['badges'],
     queryFn: () => axiosClient.get('/user-levels').then(({ data }) => (data.user_level)),
   })
@@ -17,24 +17,31 @@ const UserBadges = () => {
         <div className="section-header">
           <h3>Your Badges</h3>
         </div>
-        <div className="badges-card">
-          {
-            data &&
-              data.length > 0 ? data.map(level => (
-                <div className="level-container">
-                  <span><strong>/{level.community_name}</strong></span>
-                  <img src={`/${level.badge}`} alt="" />
-                  <ProgressBar completedClassName="barCompleted" className='levelProgressBar' barContainerClassName='barContainer' height='1.5rem' width={120} completed={`${level.experience_points}` || 0} maxCompleted={level.next_level_experience || 0} />
-                  {level.experience_points + '/' + level.next_level_experience}
-                  <span>Level {level.level}</span>
-                </div>
-              ))
+        {
+          !isLoading ?
+            data.length > 0 ?
+              <div className="badges-card">
+                {
+                  data &&
+                    data.length > 0 ? data.map(level => (
+                      <div className="level-container">
+                        <span><strong>/{level.community_name}</strong></span>
+                        <img src={`/${level.badge}`} alt="" />
+                        <ProgressBar completedClassName="barCompleted" className='levelProgressBar' barContainerClassName='barContainer' height='1.5rem' width={120} completed={`${level.experience_points}` || 0} maxCompleted={level.next_level_experience || 0} />
+                        {level.experience_points + '/' + level.next_level_experience}
+                        <span>Level {level.level}</span>
+                      </div>
+                    ))
+                    :
+                    (<div className="no-badges-text">No badges</div>)
+                }
+              </div>
               :
-              (<div className="no-badges-text">No badges</div>)
+              <div><h2>No Badges</h2></div>
+            :
+            <Loading />
+        }
 
-          }
-
-        </div>
 
       </div>
       <div className="recommended"></div>
