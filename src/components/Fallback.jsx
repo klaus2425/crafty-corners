48df70d4f33b60f3d02b3a2652ed44d9bd6e7d80
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-04-18 11:44:13 +0800

    Create Fallback.jsx

---
+++ src/components/Fallback.jsx
@@ -0,0 +1,12 @@
+const Fallback = () => {
+  return (
+    <div className="authenticated-container" id='home'>
+        <div id="feed" className="feed">
+        </div>
+        <div className="recommended">
+        </div>
+    </div>
+)
+}
+
+export default Fallback;
