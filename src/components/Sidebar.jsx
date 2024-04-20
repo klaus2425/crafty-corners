938c6fba258e13a4442e5fb9516c13eb776fbfad
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-04-20 16:39:14 +0800

    Fix profile navigation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,6 @@
 import { useQueryClient } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import axiosClient from '../axios-client';
 import { useStateContext } from '../context/ContextProvider';
 import echo from './Echo';
 
@@ -79,7 +78,7 @@ export const Sidebar = () => {
                     <Link to={'/Notifications'}>Notifications</Link>
                 </div>
                 <div className="sidebar-link" onClick={() => {
-                    navigate(`/profile`)
+                    navigate(`/profile`, {state: {id: user.id}})
                 }}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                         <path d="M19.6515 19.4054C20.2043 19.2902 20.5336 18.7117 20.2589 18.2183C19.6533 17.1307 18.6993 16.1749 17.4788 15.4465C15.907 14.5085 13.9812 14 12 14C10.0188 14 8.09292 14.5085 6.52112 15.4465C5.30069 16.1749 4.34666 17.1307 3.74108 18.2183C3.46638 18.7117 3.79562 19.2902 4.34843 19.4054C9.39524 20.4572 14.6047 20.4572 19.6515 19.4054Z" fill="#222222" />
