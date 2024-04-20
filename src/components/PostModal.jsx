27295241a769f1d1576b37af18b3385f495e1786
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-04-20 10:05:07 +0800

    Disable loading on error

--- src/components/PostModal.jsx
+++ src/components/PostModal.jsx
@@ -58,6 +58,7 @@ const PostModal = (props) => {
           },
           error: (err) => {
             setIsButtonDisabled(false);
+            setLoading(false)
             return `${err.response.data.message}`
           },
         },
