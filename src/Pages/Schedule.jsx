1893b9d5696c07039d474e0dd99b0b6ac2fa4fcf
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-04-20 16:01:43 +0800

    Adjust display

--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -1,8 +1,7 @@
 import axiosClient from '../axios-client';
 import { useState, useEffect, Suspense, lazy } from 'react'
-import EditSchedule from '../components/EditSchedule';
 import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import dayGridPlugin from '@fullcalendar/daygrid' 
 import interactionPlugin from "@fullcalendar/interaction"
 import getCorrectColor from '../components/utils/ColorCorrection';
 
