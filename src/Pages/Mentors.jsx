cd4474ce8466fe3151242fef7e7cc88bc358b3f4
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-04-26 19:08:42 +0800

    Clean up code

--- src/Pages/Mentors.jsx
+++ src/Pages/Mentors.jsx
@@ -29,7 +29,6 @@ const Mentors = () => {
                 <div className='section-header-col'>
                 </div>
                 <div className="mentor-card">
-                    
                     <div className="apply-mentor-btn-container">
                     <div className="flex mentor-header">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
